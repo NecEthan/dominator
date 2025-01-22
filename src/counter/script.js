954405ca57b61ca9da78364b77544b7f9b0a8b2f
GLOBAL\ethan.wicks <ethan.wicks@necsws.com>
2025-01-22 11:08:14 +0000

    add

--- src/counter/script.js
+++ src/counter/script.js
@@ -1,1 +1,15 @@
 // TODO: increment and decrement the count when the buttons are clicked
+
+const plus = document.getElementById('increment');
+const minus = document.getElementById('decrement');
+const count = document.getElementById('count');
+
+plus.addEventListener('click', () => {
+    let value = parseInt(count.innerText); 
+    count.innerText = value + 1;
+})
+
+minus.addEventListener('click', () => {
+    let value = parseInt(count.innerText); 
+    count.innerText = value - 1;
+})
